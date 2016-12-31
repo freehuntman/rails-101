c973e3a0da3eb31d40d6a1a59dff196dfc1433a2
吴海铭 <freehuntman@gmail.com>
2017-01-01 01:18:47 +0800

    replace form with simple_form bootstrap template

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,16 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def create
+   @group = Group.new(group_params)
+
+   if @group.save
+     redirect_to groups_path
+   else
+     render :new
+   end
+ end
+
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
@@ -22,14 +32,6 @@ class GroupsController < ApplicationController
     else
       render :edit
     end
-  end
-
-  def update
-    @group = Group.find(params[:id])
-
-    @group.update(group_params)
-
-    redirect_to groups_path, notice: "Update Success"
   end
 
   def destroy
